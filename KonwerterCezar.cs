50ebfdb6384bc0107ba586b8b536f64fe9a79258
nemesisevil <wojciech.baranski@interia.pl>
2016-12-14 16:38:09 +0100

    1.0
    Program encrypts text entered by the user

---
+++ KonwerterCezar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szyfr
+{
+    class KonwerterCezar
+    {
+        private string _szyfr = "null";
+        string poszyfr = null;
+        int _przesun;
+
+        public KonwerterCezar(string Szyfr, int Przes)  //konstruktor
+        {
+            _szyfr = Szyfr;
+            _przesun = Przes;
+
+        }
+
+        public void Szyfruj()  // szyfruje
+        {
+
+            for(int i=0; i<_szyfr.Length; i++)
+            {
+                int index = _szyfr.ElementAt(i);  // nr znaku aktualnie pobranego
+                int x = 0;
+                x = 93 + (index + _przesun - 93) % 26;
+                poszyfr += Convert.ToString((char)x);
+
+            }
+        }
+
+        public string Konwersja()
+        {
+            Szyfruj();
+            return poszyfr;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Szyfr
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string schowek = null;
+        int przesuniecie = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void przycKonwert_Click(object sender, RoutedEventArgs e)
+        {
+            schowek = Convert.ToString(Textbox.Text);
+            przesuniecie = int.Parse(liczbaBox.Text);
+            KonwerterCezar Szyfruj = new KonwerterCezar(schowek , przesuniecie);
+            Wyswietlacz.Content = "Zaszyfrowany tekst: " + Szyfruj.Konwersja();  //- wyswietlanie
+        }
+
+    }
+}
